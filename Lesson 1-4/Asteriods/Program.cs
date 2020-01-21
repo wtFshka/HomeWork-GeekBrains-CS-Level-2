113b8c51e5f3f2fbdb55fdff7c01f03368a6ad7d
Sergey Deev <sdeev5@mvd.ru>
2020-01-21 16:55:32 +0300

    Добавьте файлы проекта.

---
+++ Lesson 1-4/Asteriods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteriods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
